0ec40d8cad9cf796cba0034c7aa1a58bc4e4a5cc
Arshad <arshad.mk1612@gmail.com>
2021-05-30 16:05:56 +0530

    first push from eclipse

---
+++ devops_jenkins/src/test/java/devops_jenkins/TestHelloWorld.java
@@ -0,0 +1,11 @@
+package devops_jenkins;
+
+import org.junit.Test;
+
+public class TestHelloWorld {
+
+	@Test
+	public void testHelloWorld() {
+		System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>   Hello World   <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+	}
+}
